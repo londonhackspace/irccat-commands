8354dca09f7011b2359883eb8f4ae3e024b2522f
Jonty Wareing <jonty@jonty.co.uk>
2010-05-31 19:00:56 +0100

    Add keyholders script, parses the wikipage....badly. I'm fucking lazy.

---
+++ keyholders.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+import urllib2, re
+
+url = "http://wiki.hackspace.org.uk/w/index.php?title=Keyholders&printable=yes"
+response = urllib2.urlopen(url)
+data = response.read()
+
+match = re.search('Current.*?<ul>(.*?)</ul>', data, re.DOTALL)
+people = match.group(1)
+
+# Woo, evil!
+peopleList = re.findall('<li>\s*(?:<a.*?>)?\s*(.*?)\s*(?:</a>)?\s*</li>', people, re.DOTALL);
+
+print 'Current Keyholders: ' + ', '.join(peopleList)
