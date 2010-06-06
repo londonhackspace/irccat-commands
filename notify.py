8dcfe666ed2f7b129465088ac38631fe215a14b5
Jonty Wareing <jonty@jonty.co.uk>
2010-06-06 18:16:26 +0100

    Sol fixed the bug that required the sleeping, and the board now supports all of basic ASCII

--- notify.py
+++ notify.py
@@ -6,7 +6,6 @@ try:
     port = serial.Serial("/dev/ttyUSB1", 9600, timeout=1)
 except:
     print "Could not connect to arduino"
-    #port.close() //don't need to close if it couldnt open
     sys.exit(0)
 
 args = sys.argv
@@ -15,7 +14,7 @@ message = " ".join(args[5::])
 #Shorten message to 21 chars or less
 message = message[:21]
 
-if re.match('^[a-zA-Z0-9 ]+$', message):
+if message == '' or re.match('^[ -~]+$', message):
     message = message
 else:
     print 'Alphanumeric only please'
@@ -29,7 +28,5 @@ if len(message) < 21:
     message = message + "|"
 
 #Send to arduino
-time.sleep(2)
 port.write(message)
-time.sleep(1)
 port.close()
