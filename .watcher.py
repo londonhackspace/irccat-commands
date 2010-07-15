9ccde4f1bed2170f69880129e5e610318686fb6d
Mark Steward <marksteward@gmail.com>
2010-07-15 01:02:40 +0100

    Watch auctions on eBay

---
+++ .watcher.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import time
+import pickle
+from urllib import urlopen, urlencode
+from telnetlib import Telnet
+
+robonaut = Telnet('127.0.0.1', 12345)
+
+picklefile = '/usr/share/irccat/.watches.pck'
+
+def get_price(itemid):
+  api_args = dict(
+    callname = 'GetItemStatus',
+    responseencoding = 'NV',
+    appid = 'Tormente-ea98-4cb9-bfca-11339ff21b58',
+    siteid = 3,
+    version = 515,
+    itemid = itemid,
+  )
+
+  url = 'http://open.api.ebay.com/shopping?%s' % urlencode(api_args)
+
+  f = urlopen(url)
+  d = f.readline()
+  d = [x.split('=') for x in d.split('&')]
+  d = dict(d)
+
+  ccy   = d['Item(0).ConvertedCurrentPrice.CurrencyID']
+  price = d['Item(0).ConvertedCurrentPrice.Value']
+
+  ccyfs = dict(
+    GBP = '£%s',
+    USD = '$%s',
+  )
+
+  try:
+    ccyf = ccyfs[ccy]
+  except KeyError:
+    raise Exception('Unexpected currency %s' % ccy)
+
+  if price is None:
+    raise Exception('No price returned')
+
+  return ccyf % price
+
+
+def get_watches():
+  f = open(picklefile, 'rb')
+  watches = pickle.load(f)
+  f.close()
+  return watches
+
+def save_watches(watches):
+  f = open(picklefile, 'wb')
+  pickle.dump(watches, f)
+  f.close()
+
+
+while True:
+  try:
+    watches = get_watches()
+
+  except Exception, e:
+    print e
+    time.sleep(1)
+    continue
+
+
+  for itemid, watch in watches.items():
+    try:
+      price = get_price(itemid)
+
+    except Exception, e:
+      print e
+      continue
+
+    if 'oldprice' not in watch:
+      robonaut.write('Current %s Price: %s\n' % (watch['name'], price))
+
+    elif price != watch['oldprice']:
+      robonaut.write('Alert! Price of %s Now: %s\n' % (watch['name'], price))
+
+    watch['oldprice'] = price
+
+  try:
+    save_watches(watches)
+  except Exception, e:
+    print e
+
+  time.sleep(5)
+
+
---
+++ watch.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import sys
+import pickle
+
+itemid = sys.argv[5]
+name = sys.argv[6]
+
+picklefile = '/usr/share/irccat/.watches.pck'
+
+def get_watches():
+  f = open(picklefile, 'rb')
+  watches = pickle.load(f)
+  f.close()
+  return watches
+
+def save_watches(watches):
+  f = open(picklefile, 'wb')
+  pickle.dump(watches, f)
+  f.close()
+
+if itemid is None or name is None:
+  sys.exit()
+
+try:
+  watches = get_watches()
+except:
+  watches = {}
+
+if itemid in watches:
+  print 'Item already being watched'
+  sys.exit()
+
+watches[itemid] = { 'name': name }
+
+save_watches(watches)
