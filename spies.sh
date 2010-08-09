f149fbc62b83994a2f54dde566fc50d31edb8a38
Adrian Godwin <artag@toynbee.org.uk>
2010-08-09 23:27:14 +0100

    irccat seems to insert line breaks if multiple 'echo()' is used

--- spies.sh
+++ spies.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 # count the active connections to the webcams
 
+summary=""
+
 streams()
 {
-  echo -n $1
-  echo -n " stream"
-  if [ $1 != 1 ] ; then echo -n s ; fi
-  echo -n " $2"
+  summary="${summary} $1 stream"
+  if [ $1 != 1 ] ; then summary="${summary}s" ; fi
+  summary="${summary} $2"
 }
 
 
 set $(host $(hostname))
-streams $(netstat -n | grep ${4}:8001 | wc -l) "on main webcam, " 
-streams $(netstat -n | grep ${4}:8002 | wc -l) "on robot webcam." 
-echo
+streams $(netstat -n | grep ${4}:8001 | wc -l) "on main webcam," 
+streams $(netstat -n | grep ${4}:8002 | wc -l) "on robot webcam.\n" 
+
+echo -en $summary
 
 
