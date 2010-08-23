679f7b6932dbadf7734c12adb17a07bc47ebad85
Mark Steward <marksteward@gmail.com>
2010-08-23 03:22:18 +0100

    We need pertitent definitions for certain words/abbreviations

---
+++ define.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+import urllib
+import htmllib
+import formatter
+import sys
+import json
+import re
+
+def getjson(base, args):
+  
+  url = base + urllib.urlencode(args)
+  f = urllib.urlopen(url)
+  d = json.loads(f.read())
+  return d
+
+def unescapehtml(s):
+  f = formatter.NullFormatter()
+  p = htmllib.HTMLParser(f)
+  p.save_bgn()
+  p.feed(tidy(s))
+  return p.save_end()
+
+def tidy(output):
+  return re.sub('<br/?>', '\n', re.sub('\r', '', output))
+
+
+
+if len(sys.argv) < 6 or sys.argv[5] is None:
+  sys.exit()
+
+term = ' '.join(sys.argv[5:])
+args = {'term': term}
+
+
+define = getjson('http://www.urbandictionary.com/iphone/search/define?', args)
+#related = getjson('http://www.urbandictionary.com/iphone/search/related?', args)
+
+try:
+  sense = define['list'][0]
+
+  defn = sense['definition']
+  ex = sense['example']
+except KeyError:
+  sys.exit()
+
+
+firstline = unescapehtml(defn).splitlines()[0]
+if len(firstline) > 200:
+  firstline = firstline[0:200] + '...'
+print firstline
+#print tidy(ex)
+
+
+
+
