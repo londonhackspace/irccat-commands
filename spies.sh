dae260e82ca4452e591dda8edb9f86dd1fdf8a35
Adrian Godwin <artag@toynbee.org.uk>
2010-09-29 17:59:46 +0100

    Add counter for doorcam

--- spies.sh
+++ spies.sh
@@ -5,15 +5,17 @@ summary=""
 
 streams()
 {
-  summary="${summary} $1 stream"
+  summary="${summary}$1 stream"
   if [ $1 != 1 ] ; then summary="${summary}s" ; fi
   summary="${summary} $2"
 }
 
 
 set $(host $(hostname))
-streams $(netstat -n | grep -c "${4}:8001.*ESTABLISHED" ) "on main webcam," 
-streams $(netstat -n | grep -c "${4}:8002.*ESTABLISHED" ) "on robot webcam.\n" 
+streams $(netstat -n | grep -c "${4}:8001.*ESTABLISHED" ) "on main, " 
+streams $(netstat -n | grep -c "${4}:8002.*ESTABLISHED" ) "on robot, " 
+streams $(netstat -n | grep -c "${4}:8003.*ESTABLISHED" ) "on door cam.\n" 
+# streams $(netstat -n | grep -c "${4}:800.*CLOSE_WAIT"   ) "closing.\n" 
 
 echo -en $summary
 
