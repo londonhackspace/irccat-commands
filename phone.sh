e7e41dd3859eb51b7b5d18423f68ddd2ca135bb7
Mark Steward <marksteward@gmail.com>
2010-10-05 18:06:58 +0100

    Cover more number formats

--- phone.sh
+++ phone.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 if [ "$5" != '' ]; then
-  whois $5|grep 'phone\:\|+[0-9]'|head -1|sed 's/phone\:\s*//'|sed 's/\(+[^+]*\)+/\1/'|sed 's/^ *\([^ ]*\) *$/\1/'
+  whois $5|grep 'phone\:\|+[0-9]\|\([0-9]\{3,\}\) \{3,\}'|head -1|sed 's/phone\:\s*//'|sed 's/\(+[^+]*\)+/\1/'|sed 's/^ *\([^ ]*\) *$/\1/'
 fi
