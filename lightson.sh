0035227c96ed11032883e34d7c44105a290ffced
Mark Steward <marksteward@gmail.com>
2010-10-05 18:06:31 +0100

    Accept HTML style colours

--- lightson.sh
+++ lightson.sh
@@ -2,8 +2,11 @@
 
 COLOUR=$5
 
+
 if [[ ! $COLOUR ]]; then
     COLOUR='ffffff'
 fi
+
+COLOUR=`echo $COLOUR|sed 's/^#//'`
 
 curl -s http://localhost:8000/_/$COLOUR -o /dev/null && echo "Lights are now $COLOUR"
