51bfd8f6d0642b7cb6b6e111da6fff978b50a73d
Mark Steward <marksteward@gmail.com>
2010-10-07 11:27:12 +0100

    Make ping useful

--- ping.sh
+++ ping.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-echo "Pong"
+
+shift; shift; shift; shift
+
+if [ $# != 1 ]; then
+  echo Pong!
+  exit
+fi
+
+ping -c1 $1 2>/dev/null 1>/dev/null
+if [ "$?" == '0' ]; then
+  echo $1 is alive
+else
+  echo $1 is not alive
+fi
