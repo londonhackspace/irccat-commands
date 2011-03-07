4008e5cb32ef7fa9cd95b2cbcd512c653c965815
Mark Steward <marksteward@gmail.com>
2011-03-07 11:33:39 +0000

    Deal with empty response from lhs_spacensus.sh

--- people.sh
+++ people.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+
 PEOPLE=`/var/www/cacti/scripts/lhs_spacensus.sh | grep -E -o '[0-9]+'`
-echo "Approximately $PEOPLE people are in the space.  (Graphs: http://bit.ly/g5bxbQ)"
+if [ "$PEOPLE" != '' ]; then
+  echo "Approximately $PEOPLE people are in the space."
+fi
