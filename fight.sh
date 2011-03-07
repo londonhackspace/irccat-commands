c4849eaa802390e31e0c9c5ae7b15059219cf0f5
Mark Steward <marksteward@gmail.com>
2011-03-07 11:33:06 +0000

    Deal with empty response from hits

--- fight.sh
+++ fight.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-
-echo `/usr/share/irccat/hits.py $1 $2 $3 $4 $5` vs. `/usr/share/irccat/hits.py $1 $2 $3 $4 $6`
+a=`/usr/share/irccat/hits.py $1 $2 $3 $4 $5`
+b=`/usr/share/irccat/hits.py $1 $2 $3 $4 $6`
+if [ "$a" != '' -a "$b" != '' ]; then
+  echo $a vs. $b
+fi
