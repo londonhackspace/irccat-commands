dab3ad21c7f34870323b4ee036d68f763808c7f1
Mark Steward <marksteward@gmail.com>
2011-04-03 16:55:00 +0100

    Update for Google translate changes

--- babel.py
+++ babel.py
@@ -4,10 +4,21 @@ import sys, re
 from urllib import urlencode
 from urllib2 import urlopen, Request
 import simplejson as json
+import ConfigParser
 
 args = sys.argv[5:]
 if not args:
   sys.exit()
+
+config = ConfigParser.ConfigParser()
+config.read((
+    'babel.conf',
+    sys.path[0] + '/babel.conf',
+    '/etc/babel.conf'
+))
+
+apikey = config.get('babel', 'key')
+
 
 if '|' in args[0]:
   langpair = args.pop(0)
@@ -16,13 +27,15 @@ else:
 
 if False:
   # Doesn't work with Latin yet (as that's alpha)
+  sl, pipe, tl = langpair.partition('|')
   params = {
-    'v': '1.0',
-    'q': ' '.join(args),
-    'langpair': langpair,
+      'key': apikey,
+      'q': ' '.join(args),
+      'source': sl,
+      'target': tl,
   }
   qs = urlencode(params)
-  url = 'http://ajax.googleapis.com/ajax/services/language/translate?' + qs
+  url = 'https://www.googleapis.com/language/translate/v2?' + qs
   res = json.loads(urlopen(url).read())
   #print res
   data = res.get('responseData')
@@ -47,6 +60,9 @@ else:
     'text': ' '.join(args),
     'sl': sl,
     'tl': tl,
+    #'hl': 'en',
+    'sc': 1, # suggest
+    #'multires': 1,
   }
   qs = urlencode(params)
   headers = {
@@ -54,18 +70,36 @@ else:
     'User-Agent': 'Mozilla/5.0 (X11; U; Linux i686)' # From urllib2 docs
                   ' Gecko/20071127 Firefox/2.0.0.11',
   }
-  req = Request('http://translate.google.co.uk/translate_a/t?', qs, headers)
+  req = Request('http://translate.google.com/translate_a/t?', qs, headers)
   res = urlopen(req).read()
   #print res
-  res = json.loads(res.replace(',,',',null,'))
-  phrases, senses, lang = res
-  text = ''.join(tt for tt, st, dunno in phrases)
-  if True and not sl and lang != 'en':
-    print '%s (%s)' % (text, lang.title())
+  res = res.replace(',,',',null,')
+  res = res.replace(',,',',null,')
+  res = res.replace(',]',',null]')
+  #print res
+  res = json.loads(res)
+
+  lines, senses, lang, parsing, analysis, alternates = res[:6]
+  #print res
+  u1, didyoumean, u2, somenum, u3 = res[6:11]
+  text = ''.join(tt for tt, st, u3, standalone in lines)
+  if analysis:
+    # I think u3 might be popularity
+    t = [(tt, sp) for tt, alts, sp, u2, u3, wfrom, wto, line in analysis]
+    text = ''.join('%s%s' % (' ' if sp == 1 else '', tt) for tt, sp in t)
+    text = text.lstrip(' ')
+
+  if not sl and lang != 'en':
+    msg = '%s (%s)' % (text, lang.title())
   else:
-    print text
+    msg = text
 
+  if didyoumean:
+    html, text = didyoumean
+    #html = html.decode('unicode-escape')
+    msg = '%s (Did you mean %s?)' % (msg, text)
 
+  print msg
 
 #match = re.search('"estimatedResultCount":"(.*?)"', page)
 #if match:
