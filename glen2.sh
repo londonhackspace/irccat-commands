b8fd6fb56027b1e9929d3bb32a099e342bc0ed2b
Mark Steward <marksteward@gmail.com>
2012-03-07 12:45:53 +0000

    Make glen2 script PM

--- glen2.sh
+++ glen2.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ "$2" != 'null' ]; then
+    echo "$1, I have pm'd you the status of ALL the machines."
+fi
+
+STATUSES=`
 /usr/share/irccat/makerbot.sh
 /usr/share/irccat/layzor.sh
 /usr/share/irccat/3in1.sh
@@ -7,3 +12,8 @@
 /usr/share/irccat/printer.sh
 /usr/share/irccat/knitter.sh
 /usr/share/irccat/stratasys.sh
+`
+
+echo -en "@$1 $STATUSES" | nc -q0 localhost 12345
+
+
