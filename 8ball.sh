f85754bba00e5b64f3649a7c7275ea001e76f100
Mark Steward <marksteward@gmail.com>
2012-12-25 21:06:28 +0000

    Remove arguments to 8ball
    People tend to ask "?8ball will I rich", so returning "I" is a bit odd.

--- 8ball.sh
+++ 8ball.sh
@@ -1,30 +1,27 @@
 #!/usr/bin/env python
 import random, sys
 
-if len(sys.argv) > 5:
-    choices = sys.argv[5:]
-else:
-    choices = [
-    'As I see it, yes',
-    'It is certain',
-    'It is decidedly so',
-    'Most likely',
-    'Outlook good',
-    'Signs point to yes',
-    'Without a doubt',
-    'Yes',
-    'Yes - definitely',
-    'You may rely on it',
-    'Reply hazy, try again',
-    'Ask again later',
-    'Better not tell you now',
-    'Cannot predict now',
-    'Concentrate and ask again',
-    'Don\'t count on it',
-    'My reply is no',
-    'My sources say no',
-    'Outlook not so good',
-    'Very doubtful',
-    ]
+choices = [
+'As I see it, yes',
+'It is certain',
+'It is decidedly so',
+'Most likely',
+'Outlook good',
+'Signs point to yes',
+'Without a doubt',
+'Yes',
+'Yes - definitely',
+'You may rely on it',
+'Reply hazy, try again',
+'Ask again later',
+'Better not tell you now',
+'Cannot predict now',
+'Concentrate and ask again',
+'Don\'t count on it',
+'My reply is no',
+'My sources say no',
+'Outlook not so good',
+'Very doubtful',
+]
 
 print random.choice(choices)
