088929821e28ca96a5782b929f6d0f180ee6e63e
Mark Steward <marksteward@gmail.com>
2013-07-17 20:04:17 +0100

    Add ROT13 detection and fix morse support

--- babel.py
+++ babel.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import sys, re
+import sys, re, codecs
 from urllib import urlencode
 from urllib2 import urlopen, Request
 import simplejson as json
@@ -12,8 +12,8 @@ if not args:
 
 config = ConfigParser.ConfigParser()
 config.read((
-    'babel.conf',
-    sys.path[0] + '/babel.conf',
+    '.babel.conf',
+    sys.path[0] + '/.babel.conf',
     '/etc/babel.conf'
 ))
 
@@ -25,19 +25,20 @@ else:
   langpair = '|en'
 
 text = ' '.join(args)
-if re.match(r'[.\- ]+', text):
+if re.match(r'[.\- ]+', text) and text:
   import subprocess
   proc = subprocess.Popen('/usr/games/morse -ds -- %s' % text, shell=True, stdout=subprocess.PIPE)
   text = ' '.join(proc.communicate()[0].splitlines())
 
-if langpair.split('|')[1] == 'morse':
+if langpair.split('|')[1] == 'morse' and text:
   import subprocess
   proc = subprocess.Popen('/usr/games/morse -s -- %s' % text, shell=True, stdout=subprocess.PIPE)
   text = ''.join(proc.communicate()[0].splitlines())
   print text
   sys.exit()
 
-if False:
+
+def translate_api(langpair, text):
   # Doesn't work with Latin yet (as that's alpha)
   sl, pipe, tl = langpair.partition('|')
   params = {
@@ -65,53 +66,73 @@ if False:
     else:
       print 'API failure'
 
-else:
+def translate_web(langpair, text):
   sl, pipe, tl = langpair.partition('|')
-  params = {
-    'client': 't',
-    'text': text,
-    'sl': sl,
-    'tl': tl,
-    #'hl': 'en',
-    'sc': 1, # suggest
-    #'multires': 1,
-  }
-  qs = urlencode(params)
-  headers = {
-    'Referer': 'http://translate.google.com?hl=en',
-    'User-Agent': 'Mozilla/5.0 (X11; U; Linux i686)' # From urllib2 docs
-                  ' Gecko/20071127 Firefox/2.0.0.11',
-  }
-  req = Request('http://translate.google.com/translate_a/t?', qs, headers)
-  res = urlopen(req).read()
-  #print res
-  res = res.replace(',,',',null,')
-  res = res.replace(',,',',null,')
-  res = res.replace(',]',',null]')
-  #print res
-  res = json.loads(res)
 
-  lines, senses, lang, parsing, analysis, alternates = res[:6]
-  #print res
-  u1, didyoumean, u2, somenum = res[6:11]
-  text = ''.join(tt for tt, st, u3, standalone in lines)
-  if analysis:
-    # I think u3 might be popularity
-    t = [(tt, sp) for tt, alts, sp, u2, u3, wfrom, wto, line in analysis]
-    text = ''.join('%s%s' % (' ' if sp == 1 else '', tt) for tt, sp in t)
-    text = text.lstrip(' ')
+  def _translate_web(text, sl, tl):
+
+    params = {
+      'client': 't',
+      'text': text,
+      'sl': sl,
+      'tl': tl,
+      #'hl': 'en',
+      'sc': 1, # suggest
+      #'multires': 1,
+    }
+    qs = urlencode(params)
+    headers = {
+      'Referer': 'http://translate.google.com?hl=en',
+      'User-Agent': 'Mozilla/5.0 (X11; U; Linux i686)' # From urllib2 docs
+                    ' Gecko/20071127 Firefox/2.0.0.11',
+    }
+    req = Request('http://translate.google.com/translate_a/t?', qs, headers)
+    res = urlopen(req).read()
+    #print res
+    res = res.replace(',,',',null,')
+    res = res.replace(',,',',null,')
+    res = res.replace(',]',',null]')
+    #print res
+    res = json.loads(res)
+
+    lines, senses, lang, parsing, analysis, alternates = res[:6]
+    #print res
+    u1, didyoumean, u2, score = res[6:11]
+    text = ''.join(tt for tt, st, u3, standalone in lines)
+    if analysis:
+      # I think u3 might be popularity
+      t = [(tt, sp) for tt, alts, sp, u2, u3, wfrom, wto, line in analysis]
+      text = ''.join('%s%s' % (' ' if sp == 1 else '', tt) for tt, sp in t)
+      text = text.lstrip(' ')
+
+    return text, lang, score, didyoumean
+
+  textxl, lang, score, didyoumean = _translate_web(text, sl, tl)
+
+  if textxl == text:
+    textrot13 = codecs.encode(text, 'rot13')
+    textrot13xl, langrot13, scorerot13, didyoumeanrot13 = _translate_web(textrot13, sl, tl)
+    if scorerot13 < score:
+      textxl, lang, didyoumean = textrot13xl, 'rot13, %s' % langrot13, didyoumeanrot13
 
   if not sl and lang != 'en':
-    msg = '%s (%s)' % (text, lang.title())
+    msg = '%s (%s)' % (textxl, lang.title())
   else:
-    msg = text
+    msg = textxl
 
   if didyoumean:
-    html, text = didyoumean
+    html, text = didyoumean[:2]
     #html = html.decode('unicode-escape')
     msg = '%s (Did you mean %s?)' % (msg, text)
 
   print msg.encode('utf-8')
+
+
+if False:
+    translate_api(langpair, text)
+
+else:
+    translate_web(langpair, text)
 
 #match = re.search('"estimatedResultCount":"(.*?)"', page)
 #if match:
