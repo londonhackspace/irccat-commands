d941ed59da86b06dedf4406eea9fd6bd4bb0ba77
Jonty Wareing <jonty@jonty.co.uk>
2013-07-17 19:57:55 +0100

    Sandwich recommendation engine

---
+++ sammitch.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import sys
+import codecs
+import random
+import requests
+
+from lxml import html
+
+sys.stdout = codecs.getwriter('utf8')(sys.stdout)
+
+response = requests.get('http://en.wikipedia.org/wiki/List_of_sandwiches')
+root = html.document_fromstring(response.content)
+
+# Some sandwiches are inexplicably in italics
+elements = root.xpath('//tr/td[1]/a|//tr/td[1]/i/a')
+
+sammitch = random.choice(elements)
+print "WHY DON'T YOU EAT A %s SAMMITCH: http://en.wikipedia.org%s" % (sammitch.text.strip().upper(), sammitch.attrib['href'])
