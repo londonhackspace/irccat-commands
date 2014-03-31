d509b2d7812b59ef5b09278340c0e26574ce6a6c
Mark Steward <marksteward@gmail.com>
2014-03-31 14:08:30 +0100

    Use forecast.io for weather

--- weather.py
+++ weather.py
@@ -1,46 +1,38 @@
 #!/usr/bin/env python
-from xml.dom.minidom import parseString
-try:
-    from urllib.request import urlopen
-except ImportError:
-    from urllib import urlopen
-try:
-    from urllib.parse import urlencode
-except ImportError:
-    from urllib import urlencode
-from sys import argv, exit, stderr
-from os.path import expanduser
+import requests
+import json
+from pprint import pprint
 
-VERSION = '0.3'
-URL = 'http://closure.ath.cx/cliweather'
+response = requests.get('https://api.forecast.io/forecast/5e2226c786c32cbfcd925a75f52f9494/51.531801,-0.060318?units=uk')
+data = json.loads(response.content)
 
-place = ' '.join(argv[5:])
-if not place:
-    place = 'E2 9DY'
+#pprint(data)
 
-api = 'http://www.google.com/ig/api?%s' % urlencode({'weather': place})
+current_summary = data['currently']['summary']
+temp_c = data['currently']['temperature']
+temp_f = (temp_c * 9.0 / 5) + 32
+app_temp_c = data['currently']['apparentTemperature']
+app_temp_f = (app_temp_c * 9.0 / 5) + 32
+humidity = data['currently']['humidity']
+wind_bearing = data['currently']['windBearing']
+wind_compass = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW', 'N'][int(wind_bearing / 45.0 + 0.5)]
+wind_speed = data['currently']['windSpeed']
+minute_summary = data['minutely']['summary']
+day_summary = data['daily']['summary']
 
-dom = parseString(urlopen(api).read())
+feels_like = ''
+if app_temp_c != temp_c:
+    feels_like = u' (feels like %.1f\u00b0)' % (
+        app_temp_c,
+    )
 
-items = [ 'city'
-        , 'condition'
-        , 'temp_f'
-        , 'temp_c'
-        , 'humidity'
-        , 'wind_condition' ]
-info = {}
-try:
-    for item in items:
-        info[item] = dom.getElementsByTagName(item)[0].getAttribute('data')
-except IndexError:
-    stderr.write('Invalid Area\n')
-    exit(1)
-
-#print('City: %s' % info['city'])
-msg = u'%s%s\xb0C/%s\xb0F, %s, %s' % (
-    info['condition'] and '%s ' % info['condition'] or '',
-    info['temp_c'], info['temp_f'],
-    info['humidity'],
-    info['wind_condition'],
+msg = u'Currently %.1f\u00b0C%s, humidity %s%%, wind: %s at %.0fmph. %s %s' % (
+    temp_c,
+    feels_like,
+    int(humidity * 100),
+    wind_compass,
+    wind_speed,
+    minute_summary,
+    day_summary,
 )
 print msg.encode('utf-8')
