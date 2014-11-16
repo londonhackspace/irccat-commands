715373e01bc1ac3bfa0cb6d34d78891fdcf4b7b0
root <root@babbage.hack.rs>
2014-11-16 16:07:11 +0000

    mention who set the board - jasper

--- notify.sh
+++ notify.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo `date`: $* >> /usr/share/irccat/.notify.log
+user=$1
 shift;shift;shift;shift;
-/usr/share/irccat/setBoard.py bell.lan.london.hackspace.org.uk "$*"
-
+/usr/share/irccat/setBoard.py hamming.lan.london.hackspace.org.uk "$user on IRC said : $*" >/dev/null
+/usr/share/irccat/setBoard.py wilson.lan.london.hackspace.org.uk "$user on IRC said : $*" >/dev/null
+echo Displayed on boards
--- sixteen.sh
+++ sixteen.sh
@@ -2,4 +2,4 @@
 echo `date`: $* >> /usr/share/irccat/.notify.log
 user=$1
 shift;shift;shift;shift;
-/usr/share/irccat/setBoard.py wilson.lan.london.hackspace.org.uk "$user on IRC said: $*"
+/usr/share/irccat/setSixteen.py hamming.lan.london.hackspace.org.uk "$user on IRC said: $*"
