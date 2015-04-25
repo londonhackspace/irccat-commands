02046f3c116d6612e70e7ce573c3c356339597c3
Mark Steward <marksteward@gmail.com>
2015-04-25 15:07:02 +0100

    Neuter the commands command

--- commands.sh
+++ commands.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-if [ "$2" != 'null' ]; then
-    echo "$1, I have pm'd you the list of available commands."
-fi
 
-COMMANDS=`ls -m /usr/share/irccat | sed -r -e 's/[a-z0-9]+\.(pyc|pck),\s+//g' | sed -r -e 's/\.[a-z0-9]+//g'`
-echo -en "@$1 The available commands are:\n$COMMANDS" | nc -q0 localhost 12345
-
+echo For an idea of available commands, see http://wiki.london.hackspace.org.uk/view/Robonaut
