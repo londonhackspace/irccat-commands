f6e984e4507ef5c882097ab63780e34d77f80ae7
Mark Steward <marksteward@gmail.com>
2015-04-25 14:16:20 +0100

    Update food to remove old places

--- food.py
+++ food.py
@@ -6,52 +6,19 @@ foods = sys.argv[5:]
 random.seed()
 
 PLACES = {
-  ('Lucky Chip', '31 Coate St'):
-    ['burgers'] * 3,
+  ('Bumsmiths', '31 Coate St'):
+    ['burgers', 'burger', 'pulled pork', 'slaw', 'coleslaw'],
   ('Simply The Best', '157 Hackney Road'):
     ['chips', 'fish', 'pie', 'sausage'],
-  ('Song Que', '134 Kingsland Road'):
-    ['vietnamese', 'pho', 'beer', 'tea'],
-  ('Viet Grill', '58 Kingsland Road'):
-    ['vietnamese', 'grill', 'pho', 'curry', 'noodle', 'noodles',
-    'meat', 'lamb', 'quail', 'frog', 'pork', 'chicken', 'beef', 'duck',
-    'fish', 'monkfish', 'seabass', 'catfish', 'prawns', 'crab', 'lobster',
-    'mango', 'tofu', 'kimchi',
-    'beer', 'tea'],
-  ('Troy', '124 Kingsland Road'):
-    ['sandwich', 'sandwiches', 'panini', 'paninis', 'coffee', 'breakfast'],
-  ('Prufrock', '140 Shoreditch High St'):
-    ['coffee', 'coffee'],
-  ('Allpress Espresso', '58 Redchurch St'):
-    ['coffee', 'sandwiches'],
-  ('Saf', '152-154 Curtain Road'):
-    ['vegan', 'veg', 'vegetarian'],
-  ('Red Planet Pizza', '26 Kingsland Road'):
-    ['potato', 'pizza', 'chicken', 'tomato', 'tomatoes'],
-  ('Shoreditch Kebab House', '100 Kingsland Road'):
-    ['kebab', 'meat', 'lahmacun', 'scampi', 'falafel', 'late'],
-  ('the local offie', '110 Kingsland Road'):
-    ['beer', 'beer'],
-  ('Due Sardi', '32 Kingsland Road'):
-    ['pizza', 'tomato', 'tomatoes'],
-  ('City Beverage Company', '303 Old Street'):
-    ['beer', 'beer', 'wine'],
   ('Tesco Express', '79-85 Hackney Road'):
     ['wine', 'cake', 'caek', 'jelly', 'beans', 'horsemeat', 'horses', 'horse'],
-  ('The Four Vintners', '5-9 Kingsland Road'):
-    ['wine'],
   ('Longdan Express', '25 Hackey Road'):
     ['chinese', 'beer', 'noodle', 'noodles'],
-  ('Food Hall', '374-378 Old St'):
-    ['cheese', 'ham', 'bread', 'salami', 'chorizo', 'vegetables', 'veg',
-    'carrots', 'tomatoes', 'cabbage', 'cabbages', 'homeopathy', 'homeopathics'],
   ('Hot Pot', 'Hackney Rd, nr Columbia Rd'):
     ['italian','cafe'],
-  ('Moreti', '134 Kingsland Road'):
-    ['pasta', 'sandwich', 'sandwiches', 'panini', 'paninis', 'breakfast'],
-  ('Troyganic', '138 Kingsland Road'):
-    ['pasta', 'cheese', 'nuts', 'breakfast', 'vegetarian'],
-  ('nowhere near Old St', "I'm afraid"):
+  ('Ridley Road Market', 'Dalston'):
+    ['bushmeat', 'buffalo', 'kangaroo', 'pygmy', 'crocodile'],
+  ('nowhere near Hackney Road', "I'm afraid"):
     ['sushi', 'jellied', 'eels', 'elephant', 'cat', 'dog', 'rat', 'rats', 'cockroaches', 'cockroach'],
 }
 
