4fbbd07491bd9178a22416b8bfc6fdcee078ea9d
Mark Steward <marksteward@gmail.com>
2015-05-05 16:08:50 +0100

    Fix Wolfram

--- wolfram.py
+++ wolfram.py
@@ -12,6 +12,13 @@ def find_node(root, possible_attribs):
 
     return None
 
+def find_id_node(root):
+    node = root.xpath("/queryresult/pod[@id='Input']/subpod/plaintext")
+    return node[0].text
+
+def find_non_id_node(root):
+    node = root.xpath("/queryresult/pod[@id!='Input']/subpod/plaintext")
+    return node[0].text
 
 API_KEY = 'VX9PUR-8KE9RK9A75'
 
@@ -24,12 +31,19 @@ response = requests.get('http://api.wolframalpha.com/v2/query?appid=%s&input=%s&
 root = etree.fromstring(response.content)
 
 if root.xpath("/queryresult")[0].attrib['success'] == 'true':
-    possible_questions = ('Input interpretation', 'Input')
-    question = find_node(root, possible_questions)
+    if False: # overspecific and often wrong
+        possible_questions = ('Input interpretation', 'Input')
+        question = find_node(root, possible_questions)
 
-    possible_answers = ('Current result', 'Response', 'Result')
-    answer = find_node(root, possible_answers)
+        possible_answers = ('Current result', 'Response', 'Result', 'Results', 'IP address registrant', 'Definitions')
+        answer = find_node(root, possible_answers)
+    else:
+        question = find_id_node(root)
+        answer = find_non_id_node(root)
 
-    print "%s: %s = %s" % (user, question, answer)
+    answer = answer.replace('\n', ', ')
+
+    response = "%s: %s = %s" % (user, question, answer)
+    print response.encode('utf8')
 else:
     print "%s: Sorry, I don't understand the question" % user
