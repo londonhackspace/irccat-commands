ac0890766769c4f0766a7d1570165490ceebeb66
root <root@babbage.hack.rs>
2015-05-16 20:45:28 +0100

    re-enable and allow board clearing again

--- sixteen.sh
+++ sixteen.sh
@@ -2,4 +2,8 @@
 echo `date`: $* >> /usr/share/irccat/.notify.log
 user=$1
 shift;shift;shift;shift;
-/usr/share/irccat/setSixteen.py hamming.lan.london.hackspace.org.uk "$user on IRC said: $*"
+if [ ! -z "$1" ] ; then
+	/usr/share/irccat/setSixteen.py hamming.lan.london.hackspace.org.uk "$user on IRC said: $*"
+else
+	/usr/share/irccat/setSixteen.py hamming.lan.london.hackspace.org.uk ""
+fi
