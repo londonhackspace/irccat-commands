fa481ec9ed627a6d0b158903499aa429b386f617
Mark Steward <marksteward@gmail.com>
2015-09-27 22:08:56 +0100

    Neutralise some broken scripts

--- beacon.sh
+++ beacon.sh
@@ -1,13 +1,2 @@
 #!/bin/bash
-
-echo -en "Flashing beacons!\r\n"
-
-/usr/share/irccat/setBoard.py "Beacon: $1" &> /dev/null
-
-for i in {1..5}
-do
-    curl http://localhost:8000/_/255,255,255?restoreAfter=1 &> /dev/null
-    curl http://localhost:8000/_/0,0,0?restoreAfter=1 &> /dev/null
-done
-
-/usr/share/irccat/setBoard.py "" &> /dev/null
+echo "Unhandled error: RelaysNotFound"
