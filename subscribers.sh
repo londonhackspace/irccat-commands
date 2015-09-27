ee1b0027638d3d08f285c3cd9454ee7c8b82078a
Mark Steward <marksteward@gmail.com>
2015-09-27 22:29:06 +0100

    Show last payment time for subscriber count

--- subscribers.sh
+++ subscribers.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-COUNT=$(curl -sk https://london.hackspace.org.uk/member_stats.php|cut -d' ' -f1|cut -d: -f2)
-echo "London Hackspace currently has $COUNT paying members."
+STATS=$(curl -sk https://london.hackspace.org.uk/member_stats.php)
+COUNT=$(echo $STATS|cut -d' ' -f1|cut -d: -f2)
+TIMESTAMP=$(echo $STATS|cut -d' ' -f3|cut -d: -f2)
+LAST_TXN=$(date -d@$TIMESTAMP '+%e %B')
+echo "London Hackspace has $COUNT paying members (last payment $LAST_TXN). See some at https://london.hackspace.org.uk/members/"
