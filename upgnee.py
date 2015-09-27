f140a56f85dcd99a7a7693738b0624361309ebf6
Mark Steward <marksteward@gmail.com>
2015-09-27 22:48:14 +0100

    Add upgnee (by tomw) and upword (by yvanj)

---
+++ upgnee.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+from datetime import timedelta
+import math
+
+if sys.argv[2] != 'null':
+    print 'upword is private message only'
+    sys.exit()
+
+
+with open("/proc/uptime", "r") as f:
+   seconds = int(float(f.readline().split()[0]) / 60 / 60 /24)
+
+st = "GN" + "E"*seconds
+print st
