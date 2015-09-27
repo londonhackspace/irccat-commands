ae3d7946aee818efacae4bf00a07be65f710af8e
Mark Steward <marksteward@gmail.com>
2015-09-27 22:27:11 +0100

    Update sink script

--- sink.sh
+++ sink.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # ok, it's a guess .. but how often is it going to be wrong ?
-echo The sink needs cleaning. 
-(sleep 3; echo The toilet\'s not that great either.) &
+echo The sink is shiny and clean, every plate is tidy.
+(sleep 3; echo not.) &
 
