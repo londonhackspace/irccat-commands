c82ce5e2e07b2e53401c6f846de0e513efbcc35a
Mark Steward <marksteward@gmail.com>
2015-09-27 22:22:34 +0100

    Slightly better phone detection

--- phone.sh
+++ phone.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 if [ "$5" != '' ]; then
-  whois $5|grep 'phone\:\|+[0-9]\|\([0-9]\{3,\}\) \{3,\}'|head -1|sed 's/phone\:\s*//'|sed 's/\(+[^+]*\)+/\1/'|sed 's/^ *\([^ ]*\) *$/\1/'
-else
-  echo 020 7613 5391
+  whois $5|grep -i 'phone\:\|+[0-9]\|\([0-9]\{3,\}\) \{3,\}'|head -1|sed 's/.*phone\:\s*//i'|sed 's/\(+[^+]*\)+/\1/'|sed 's/^ *\([^ ]*\) *$/\1/'
 fi
