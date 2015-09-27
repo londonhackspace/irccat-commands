01c7b35719cacc7c4d59e48cb4d438347b1c7f9d
Mark Steward <marksteward@gmail.com>
2015-09-27 22:11:34 +0100

    Remove unused board

--- board2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo `date`: $* >> /usr/share/irccat/.notify.log
-user=$1
-shift;shift;shift;shift;
-/usr/share/irccat/setBoard.py wilson.lan.london.hackspace.org.uk "$user on IRC said: $*"
