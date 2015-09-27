56f83efe48a44edb0d833786a3a3b24a4d0fd6ce
Mark Steward <marksteward@gmail.com>
2015-09-27 22:28:36 +0100

    More substandards

--- ss.py
+++ ss.py
@@ -8,6 +8,10 @@ dates = {
   (2012,  3, 29): ('Blue Marlin', 'The Fellow', 'N1 9AA'),
   (2012, 10,  4): ('Kernel Panic', 'The Old Fountain', 'EC1V 9NU'),
   (2012, 11, 29): ('Shipping Forecast', 'The Ship Tavern', 'WC2A 3HP'),
+  (2014,  6, 19): ('Calendar', 'Bricklayers Arms', 'W1T 1QS'),
+  (2014,  7, 31): ('Parklife', 'Regents Park', 'NW1 4NR'),
+  (2015,  2, 26): ('How Do I Even', 'Cask Pub & Kitchen', 'SW1V 2EE'),
+  (2015, 12, 15): ('Decimalisation', 'UNKNOWN', 'London'),
 }
 
 def next_meet(fromd):
