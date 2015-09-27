d4fc3db0efb001faee75b787f4010dc143bb19c3
Mark Steward <marksteward@gmail.com>
2015-09-27 23:21:48 +0100

    Add suspicious meme script (by detonate)

---
+++ suspicious.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+import random
+
+suspicious = [
+    'http://i.imgur.com/cgxBjhz.jpg',
+    'http://goo.gl/KrrNCQ',
+    'http://goo.gl/AgyDB0',
+    'http://goo.gl/s4UpVu',
+    'http://goo.gl/wOjkwH',
+    'http://goo.gl/ewD2tO',
+    u'http://hack.rs/\u0ca0_\u0ca0.jpg',
+]
+print suspicious[random.randrange(len(suspicious))].encode('utf-8')
+
