4819c5ef7cb4de75132e0dfad92d07402e2d1c1e
Mark Steward <marksteward@gmail.com>
2015-09-27 22:31:41 +0100

    Make wifi show number of clients (ish)

--- wifi.sh
+++ wifi.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
-echo http://bit.ly/workingplaces
+clients=$(nmap 172.31.24.0/24 -p1 | grep -c ".dhcp.lan.london.hackspace.org.uk")
+echo There are $clients clients connected to the wifi.
