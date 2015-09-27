3ce3fe8988aa77a53829b8b7d384b6b009c64686
Mark Steward <marksteward@gmail.com>
2015-09-27 22:23:12 +0100

    PS100 is in the past now

--- ps100.py
+++ ps100.py
@@ -5,5 +5,8 @@ dates = [
   (2014,  3, 13),
 ]
 
-start, end = nextevent.get(dates, '18:00')
-nextevent.printmsg("Pub Standards 100", 'beer', start)
+try:
+    start, end = nextevent.get(dates, '12:00')
+    nextevent.printmsg("Pub Standards 100", 'party poppers and beer', start)
+except TypeError:
+    print "Pub Standards 100 was amazing."
