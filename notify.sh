26b7fd0245900ea5cb13f543c2b0ea86d2475aaf
Mark Steward <marksteward@gmail.com>
2015-09-27 22:21:17 +0100

    Update notify script to be less verbose

--- notify.sh
+++ notify.sh
@@ -2,6 +2,7 @@
 echo `date`: $* >> /usr/share/irccat/.notify.log
 user=$1
 shift;shift;shift;shift;
-/usr/share/irccat/setBoard.py hamming.lan.london.hackspace.org.uk "$user on IRC said : $*" >/dev/null
-/usr/share/irccat/setBoard.py wilson.lan.london.hackspace.org.uk "$user on IRC said : $*" >/dev/null
+# Include user as anti-trolling measure (per trustees)
+/usr/share/irccat/setBoard.py hamming.lan.london.hackspace.org.uk "<$user> $*" >/dev/null
+/usr/share/irccat/setBoard.py wilson.lan.london.hackspace.org.uk "<$user> $*" >/dev/null
 echo Displayed on boards
