55508cee97997c13e9264c2925a91c07a420b726
Mark Steward <marksteward@gmail.com>
2015-09-27 22:37:32 +0100

    BEES (by cadmus)

---
+++ bees.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+# One day this script will response with the status of the Hackspace's hives
+# But for now just have some whimsy
+
+import random
+
+responses = [
+"bees",
+"http://i.imgur.com/6dqeI.gif",
+"http://1.bp.blogspot.com/-OkI42OfJstI/T8Tx2pJkX2I/AAAAAAAAAnE/zXQjJ6zRYFQ/s320/beekeeper.jpeg.gif",
+"http://i.telegraph.co.uk/multimedia/archive/01663/radio-bee_1663308i.jpg",
+"http://www2.b3ta.com/host/creative/6327/1393875794/49beehive.png",
+"https://pbs.twimg.com/media/Bs17npoCEAApL_D.jpg:large",
+"http://i.imgur.com/nWDoM2p.jpg",
+"http://beesbeesbees.com",
+"What did the sushi say to the bee? WASABI!",
+"http://41.media.tumblr.com/5dbbbe451d75801371d7eff73b8ebcb9/tumblr_nnue63MdUa1usi9s5o1_1280.png",
+"http://www.norphil.co.uk/2015/08-Bees_of_Britain_stamps.htm",
+]
+
+print random.choice(responses)
