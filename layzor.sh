fc6bfe78cc49b1d03ec6e5bc20294f212d929991
Mark Steward <marksteward@gmail.com>
2015-09-27 22:17:41 +0100

    Acnode the laser script

--- layzor.sh
+++ layzor.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-file=/usr/share/irccat/.layzor.txt
-equipment=Layzor
-editor=$1
-
-shift; shift; shift; shift
-
-/usr/share/irccat/.equipment.sh $file $equipment $editor $*
+/usr/share/irccat/acnode.py $* laser
 
