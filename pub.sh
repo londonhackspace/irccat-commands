958b886fb0132d77829901ed493a37f7c99bcb6f
Mark Steward <marksteward@gmail.com>
2015-09-29 15:14:34 +0100

    Fix annoying typo

--- pub.sh
+++ pub.sh
@@ -4,4 +4,4 @@
 PUBDAY=`date +%A`
 KNOCKOFF="1730"
 
-echo $(($(date -d "$PUBDAY $KNOCKOFF" +%s%N) - $(date +%s%N))) nanoseconds until pub o\' clock
+echo $(($(date -d "$PUBDAY $KNOCKOFF" +%s%N) - $(date +%s%N))) nanoseconds until pub o\'clock
