6c6a7fa0c55e6e64e6f248108dd090e751aa95df
Mark Steward <marksteward@gmail.com>
2015-11-15 13:18:08 +0000

    And remove it again as it only happens once

--- bttf.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-import nextevent
-
-dates = [
-  (2015, 10, 22),  # because it's after midnight
-]
-
-start, end = nextevent.get(dates, '00:29')  # 16:29 PDT (-7), in BST (+1)
-nextevent.printmsg('The Future', 'hoverboards', start)
