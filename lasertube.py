e325dc5b0885cf64870623455aee8658667072c2
Jasper Wallace <jasper@pointless.net>
2015-12-08 22:20:09 +0000

    make lasertube kind of/sort of work again

--- lasertube.py
+++ lasertube.py
@@ -1,13 +1,31 @@
 #! /usr/bin/python
 
-print "This script is currently out of service."
-exit
+#f = open("/home/solexious/coolbot/totalTime", "r")
+#seconds = int(f.read())
 
-f = open("/home/solexious/coolbot/totalTime", "r")
-seconds = int(f.read())
+#hours = seconds / 3600
+#minutes = (seconds % 3600) / 60
+#seconds = (seconds % 3600) % 60
 
-hours = seconds / 3600
-minutes = (seconds % 3600) / 60
-seconds = (seconds % 3600) % 60
+#print '%dh:%dm:%ds of lasing have occurred. Approximately %d hours until the tube dies.' % (hours, minutes, seconds, 700 - hours)
 
-print '%dh:%dm:%ds of lasing have occurred. Approximately %d hours until the tube dies.' % (hours, minutes, seconds, 700 - hours)
+import time, datetime, urllib2, json
+
+# solexious: russ posted on the ml about the new cutter being open for use on the 24/12/2014
+
+start = datetime.datetime(2014, 12, 24)
+start = int(time.mktime(start.timetuple()))
+
+#solexious: 399h:56m:57s of lasing have occurred. was on the 18th of august
+
+#coolbot = (399 * 3600) + (56 * 60) + 57
+#start = start + coolbot
+
+# the lasercutter is toolid 5
+url = "http://acserver.london.hackspace.org.uk/api/get_tool_runtime_since/5/" + str(start)
+
+ret = urllib2.urlopen(url).read()
+ret = json.loads(ret)[0]
+
+print "using acserver data + possibly wrong coolbot time: " + ret['verbose']
+
