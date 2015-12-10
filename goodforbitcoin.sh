bf3061e57986f2d5236a2dee2ac4f53bc9e8c169
Mark Steward <marksteward@gmail.com>
2015-12-10 13:17:13 +0000

    Correct bitcoin script

--- goodforbitcoin.sh
+++ goodforbitcoin.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo This is good for bitcoin because $(shuf -n 1 /opt/irccat/irccat-data/goodforbitcoin.txt)
+echo This is good for bitcoin because $(shuf -n 1 /opt/irccat/irccat-commands/.goodforbitcoin.txt)
