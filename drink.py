1ab98b7efc69ab940d07e8c8f907bc7871c90b6c
Mark Steward <marksteward@gmail.com>
2016-06-02 15:25:12 +0000

    Add drink command

---
+++ drink.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import random
+import mmap
+import re
+
+def readwordlist(filename):
+    f = open(filename)
+    # faster than .read()
+    data = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
+    word_pattern = r'([dD].*[kK])(?=\n)'
+    word_re = re.compile(r'\n' + word_pattern)
+    wordlist = word_re.findall(data)
+    m = re.match(r'^' + word_pattern, data)
+    if m:
+        wordlist.add(m.group(1))
+    return wordlist
+
+if __name__ == '__main__':
+    wordlist = readwordlist('/usr/share/dict/british-english-insane')
+    print random.choice(wordlist).upper().encode('utf-8')
+
