7747ebbb0a26472a5e9fac2863f23135c7f074de
Jonty Wareing <jonty@jonty.co.uk>
2016-10-11 16:45:58 +0100

    Fix peopleinspace. Again.

--- peopleinspace.py
+++ peopleinspace.py
@@ -1,36 +1,17 @@
 #!/usr/bin/env python
-import urllib2, lxml.html, sys, codecs
+import sys
+import codecs
+import json
+import urllib2
 
 sys.stdout = codecs.getwriter('utf8')(sys.stdout)
 
-request = urllib2.Request('http://en.wikipedia.org/wiki/Template:People_currently_in_space')
+request = urllib2.Request('http://www.howmanypeopleareinspacerightnow.com/space.json')
 request.add_header('User-Agent', 'Zomg, 1.0')
-
 opener = urllib2.build_opener()
-site = opener.open(request).read()
+response = opener.open(request).read()
 
-# To get around a bug in lxml
-import StringIO
-site = StringIO.StringIO(site)
+data = json.loads(response)
+people = [person['name'] for person in data['people']]
 
-root = lxml.html.parse(site).getroot()
-
-amount = 0
-strings = []
-
-# If you're fixing this again, just use http://www.howmanypeopleareinspacerightnow.com/space.json
-
-
-rows = root.xpath('//table[@class="navbox"]//table[contains(@class, "navbox-inner")]/tr')
-
-for row in rows:
-    orbiter = row.xpath('./th[@class="navbox-group"]')
-
-    if orbiter:
-        astronauts = row.xpath('.//td[starts-with(@class, "navbox-list navbox-")]/div//a')
- 
-        strings.append('%s on the %s' % (len(astronauts), orbiter[0].text_content()))
-        amount += len(astronauts)
-
-
-print "There are currently %s people in space! (%s)" % (amount, ', '.join(strings))
+print "There are currently %s people in space! (%s)" % (data['number'], ', '.join(people))
