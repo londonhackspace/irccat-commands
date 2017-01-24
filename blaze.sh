09a3b709a2049f8a49bd51a055043cf6829597f5
Mark Sta Ana <booyaabooyaabooyaa@gmail.com>
2017-01-24 12:22:38 +0000

    fixed line breaks

--- blaze.sh
+++ blaze.sh
@@ -1,1 +1,3 @@
-#!/bin/bashPUBDAY=`date +%A`KNOCKOFF="1620"echo $(($(date -d "$PUBDAY $KNOCKOFF" +%s%N) - $(date +%s%N))) nanoseconds until blaze o\'clock
+#!/bin/bash
+PUBDAY=`date +%A`KNOCKOFF="1620"
+echo $(($(date -d "$PUBDAY $KNOCKOFF" +%s%N) - $(date +%s%N))) nanoseconds until blaze o\'clock
