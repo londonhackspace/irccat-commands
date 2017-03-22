a7c0e92b7c6e3dd8b2a4d43b37f9485425f75297
Mark Steward <marksteward@gmail.com>
2017-03-22 13:41:41 +0000

    Fix aesthetic some more

--- aesthetic.py
+++ aesthetic.py
@@ -8,7 +8,10 @@ if len(sys.argv) > 5:
 else:
   input_string = 'aesthetic'
 
-for c in string.ascii_letters+string.digits:
-    input_string = input_string.replace(c, unichr(ord(c)+65248))
+# See https://en.wikipedia.org/wiki/Halfwidth_and_fullwidth_forms
+for c in range(0x21, 0x7f):
+    input_string = input_string.replace(chr(c), unichr(c - 0x21 + 0xff01))
+
+input_string = input_string.replace(' ', u'\u3000')
 
 print(unicode(input_string).encode('utf-8'))
