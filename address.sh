64ddb811a33a79265c8c166202f3b11bc883adbd
zymurgic <simon.hewison@zymurgy.org>
2018-12-03 18:10:34 +0000

    Update address.sh
    found this was still referencing Hackney Road

--- address.sh
+++ address.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-echo "447 Hackney Road, E2 9DY - http://hack.rs/w/447_Hackney_Road/Getting_There"
+#echo "447 Hackney Road, E2 9DY - http://hack.rs/w/447_Hackney_Road/Getting_There"
+echo "Ujima House, 388 High Road, Wembley HA9 6AR - http://hack.rs/w/Ujima_House"
