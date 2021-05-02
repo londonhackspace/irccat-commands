ea4045966fccb5425b886dd28d34c15ddcf1b09b
Simon Hewison <simon.hewison@exactly-that.co.uk>
2021-05-02 23:36:38 +0100

    python3 syntax changes

--- 3dprintercluedo.py
+++ 3dprintercluedo.py
@@ -5,4 +5,4 @@ names = ['hipster', 'ian', 'zippo', 'dean', 'billy', 'glen']
 reasons = ['screwdriver', 'calibration', 'hammer', 'PLA', 'liquid nitrogen', 'length of wood', 'soldering iron', 'club mate bottle']
 locations = ['quiet room', 'workshop', 'classroom', 'main room', 'carpark', 'foyer']
 
-print "it was %s in the %s with the %s" % (random.choice(names), random.choice(locations), random.choice(reasons))
+print ("it was %s in the %s with the %s" % (random.choice(names), random.choice(locations), random.choice(reasons)))
--- babel.py
+++ babel.py
@@ -1,3 +1,3 @@
 #!/usr/bin/env python
 
-print "No idea."
+print ("No idea.")
